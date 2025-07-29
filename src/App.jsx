b334d56eea51901eae0933b119b342e306bfa05f
Rehan Halai <halairehan01@gmail.com>
2025-07-29 14:22:08 +0530

    Added Items Section With a bit of responsiveness

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import { Navbar } from './components/Navbar/Navbar.jsx/Navbar'
 import { HeroSection } from './components/HeroSection/HeroSection'
 import { Brands } from './components/brandLogos/Brands'
+import { Items } from './components/ItemsSection/itmes'
 
 const App = () => (
   <>
     <Navbar />
     <HeroSection />
     <Brands />
+    <Items />
   </>
 )
 
--- src/components/brandLogos/Brands.jsx
+++ src/components/brandLogos/Brands.jsx
@@ -1,7 +1,46 @@
 export const Brands = () => {
-    return (
-        <section className="Brands min-h-72 w-full bg-black">
-            
-        </section>
-    )
-}
+  return (
+    <section className="BrandsContainer h-max p-5 w-full bg-black flex justify-center items-center">
+      <div className="brands w-200 h-max  text-white font-light flex flex-col items-center ">
+        <span className="heading ">Supported Brands</span>
+        <div className="brandLogos w-full flex flex-row justify-around flex-wrap" >
+          <span className="BrandIcon">
+            <img
+              src="src\assets\SVG's\Brands\Ap.svg"
+              className="w-30 h-30"
+              alt=""
+            />
+          </span>
+          <span className="BrandIcon">
+            <img
+              src="src\assets\SVG's\Brands\HUBLOT.svg"
+              className="w-30 h-30"
+              alt=""
+            />
+          </span>
+          <span className="BrandIcon">
+            <img
+              src="src\assets\SVG's\Brands\OMEGAsvg.svg"
+              className="w-30 h-30"
+              alt=""
+            />
+          </span>
+          <span className="BrandIcon">
+            <img
+              src="src\assets\SVG's\Brands\PATEKphilipee.svg"
+              className="w-30 h-30"
+              alt=""
+            />
+          </span>
+          <span className="BrandIcon">
+            <img
+              src="src\assets\SVG's\Brands\Rolex.svg"
+              className="w-30 h-30"
+              alt=""
+            />
+          </span>
+        </div>
+      </div>
+    </section>
+  );
+};
