de8137f32c7cbaa8c8bab2cfbe674389423393bb
Rehan Halai <halairehan01@gmail.com>
2025-08-02 15:26:12 +0530

    added animated hamburger for small screens

--- src/components/Navbar/Navbar.jsx/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx/Navbar.jsx
@@ -1,6 +1,7 @@
 import { useState, useRef, useEffect, createRef } from "react";
 import { useCursorEffect } from "../../../contexts/onMouseEffectContext";
-import { motion } from "framer-motion";
+import { motion, AnimatePresence } from "framer-motion";
+import Hamburger from 'hamburger-react'
 import { Link, NavLink } from "react-router-dom";
 
 export const Navbar = () => {
@@ -14,7 +15,7 @@ export const Navbar = () => {
     navRefs.current.forEach((ref) => {
       if (ref.current) registerHoverRef(ref.current);
     });
-  }, []);
+  }, [registerHoverRef]);
 
   const navIconsImage = [
     "./assets/SVGs/Icons/search-01.svg",
@@ -26,11 +27,11 @@ export const Navbar = () => {
     navIconRefs.current.forEach((ref) => {
       if (ref.current) registerHoverRef(ref.current);
     });
-  }, []);
+  }, [registerHoverRef]);
 
   return (
     <>
-      <nav className="w-full h-16 bg-zinc-900  px-6 sm:px-16 flex items-center justify-between z-50">
+      <nav className="w-full h-16 bg-zinc-900  px-4 sm:px-16 flex items-center justify-between z-50">
         <div className="text-white text-2xl">
           Time
           <span className="text-red-600"> Zone</span>
@@ -41,6 +42,7 @@ export const Navbar = () => {
           <motion.ul className="flex justify-between gap-8">
             {navItems.map((item, index) => (
               <NavLink
+                key={item}
                 to={
                   item.toLowerCase() === "home" ? "/" : `/${item.toLowerCase()}`
                 }
@@ -48,10 +50,7 @@ export const Navbar = () => {
                   `${isActive ? "border-b-3 border-red-700" : ""}`
                 }
               >
-                <motion.li
-                  key={item}
-                  className=" `h-max w-max relative flex justify-center items-center p-1 "
-                >
+                <motion.li className=" `h-max w-max relative flex justify-center items-center p-1 ">
                   <div
                     className="absolute bounde w-full h-full hover:scale-150 "
                     ref={navRefs.current[index]}
@@ -63,10 +62,10 @@ export const Navbar = () => {
           </motion.ul>
         </div>
 
-        <div className="Nav-Icons flex gap-4">
+        <div className="Nav-Icons flex gap-2 items-center">
           {navIconsImage.map((image, index) => (
             <span
-              className="searchIcon relative w-7 h-7 mix-blend-difference"
+              className="searchIcon relative w-7 h-7 mix-blend-difference m-1"
               key={index}
             >
               <div
@@ -82,41 +81,44 @@ export const Navbar = () => {
           ))}
 
           <span
-            className="hamburger block md:hidden"
+            className="hamburger md:hidden text-white scale-60"
             onClick={() => setNavOption(!isMenuOpen)}
           >
-            <svg class="w-6 h-5 text-white fill-current" viewBox="-15 0 130 50">
-              <rect width="90" height="10"></rect>
-              <rect y="30" width="90" height="10"></rect>
-              <rect y="60" width="90" height="10"></rect>
-            </svg>
+            <Hamburger />
           </span>
         </div>
       </nav>
 
       {/* Navbar Options For Mobiles */}
-      <div
-        className={`Nav-Options ${
-          isMenuOpen ? "block" : "hidden"
-        } absolute z-10 sm:hidden bg-zinc-800 w-full text-white`}
-      >
-        <motion.ul className="flex flex-col p-4 gap-6  justify-between">
-          {navItems.map((item, index) => (
-            <NavLink
-              to={
-                item.toLowerCase() === "home" ? "/" : `/${item.toLowerCase()}`
-              }
-              className={({ isActive }) =>
-                ` p-1 ${isActive ? "border-b-3 border-red-700" : ""}`
-              }
-            >
-              <li key={item} className="">
-                {item}
-              </li>
-            </NavLink>
-          ))}
-        </motion.ul>
-      </div>
+      <AnimatePresence>
+        {isMenuOpen && (
+          <motion.div
+            initial={{ scale: 0, opacity: 0 }}
+            animate={{ scale: 1, opacity: 1 }}
+            exit={{ scale: 0, opacity: 0 }}
+            transition={{ duration: 0.5, type: "spring" }}
+            className={` absolute w-screen  z-10 sm:hidden flex flex-row-reverse p-1 origin-top-right`}
+          >
+            <ul className="flex w-[75%] relative flex-col p-4 gap-6 justify-between bg-zinc-800 text-white rounded-xl ">
+              {navItems.map((item, index) => (
+                <NavLink
+                  key={item}
+                  to={
+                    item.toLowerCase() === "home"
+                      ? "/"
+                      : `/${item.toLowerCase()}`
+                  }
+                  className={({ isActive }) =>
+                    ` py-1 ${isActive ? "border-b-[3px] border-red-700" : ""}`
+                  }
+                >
+                  <li>{item}</li>
+                </NavLink>
+              ))}
+            </ul>
+          </motion.div>
+        )}
+      </AnimatePresence>
     </>
   );
 };
--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -1,31 +1,51 @@
-
-
 export function FooterComponent() {
   return (
-    <footer class="bg-black w-full">
-    <div class="w-full max-w-screen-xl mx-auto p-4 md:py-8">
-        <div class="sm:flex sm:items-center sm:justify-between">
-            <a href="https://flowbite.com/" class="flex items-center mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse">
-                <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Time <span className="text-red-600">Zone</span></span>
-            </a>
-            <ul class="flex flex-wrap items-center mb-6 text-sm font-medium text-gray-500 sm:mb-0 dark:text-gray-400">
-                <li>
-                    <a href="#" class="hover:underline me-4 md:me-6">About</a>
-                </li>
-                <li>
-                    <a href="#" class="hover:underline me-4 md:me-6">Privacy Policy</a>
-                </li>
-                <li>
-                    <a href="#" class="hover:underline me-4 md:me-6">Licensing</a>
-                </li>
-                <li>
-                    <a href="#" class="hover:underline">Contact</a>
-                </li>
-            </ul>
+    <footer className="bg-black w-full">
+      <div className="w-full max-w-screen-xl mx-auto p-4 md:py-8">
+        <div className="sm:flex sm:items-center sm:justify-between">
+          <a
+            href="https://flowbite.com/"
+            className="flex items-center mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse"
+          >
+            <span className="self-center text-2xl font-semibold whitespace-nowrap text-white">
+              Time <span className="text-red-600">Zone</span>
+            </span>
+          </a>
+          <ul className="flex flex-wrap items-center mb-6 text-sm font-medium text-gray-500 sm:mb-0">
+            <li>
+              <a href="#" className="hover:underline me-4 md:me-6">
+                About
+              </a>
+            </li>
+            <li>
+              <a href="#" className="hover:underline me-4 md:me-6">
+                Privacy Policy
+              </a>
+            </li>
+            <li>
+              <a href="#" className="hover:underline me-4 md:me-6">
+                Licensing
+              </a>
+            </li>
+            <li>
+              <a href="#" className="hover:underline">
+                Contact
+              </a>
+            </li>
+          </ul>
         </div>
-        <hr class="my-6  border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
-        <span class="block text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2025 <a href="https://flowbite.com/" class="hover:underline">Time <span className="text-red-600">Zone</span>™</a>. All Rights Reserved.</span>
-    </div>
-</footer>
+        <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
+        <span className="block text-sm text-gray-500 sm:text-center dark:text-gray-400">
+          © 2025{" "}
+          <a
+            href="https://flowbite.com/"
+            className="hover:underline text-white"
+          >
+            Time <span className="text-red-600">Zone</span>™
+          </a>
+          . All Rights Reserved.
+        </span>
+      </div>
+    </footer>
   );
 }
